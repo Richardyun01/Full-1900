b40b334a55a17b55084361c04dce09e20e471a89
Richardyun01 <richardyun01@gmail.com>
2024-06-28 17:55:59 +0900

    add files

--- 06. React/react-app/src/App.js
+++ 06. React/react-app/src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
 import Subject from './components/Subject';
+import CreateContent from './components/CreateContent';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
+      mode:'welcome',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', sub:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,14 +23,78 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc, _article = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        // this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        var_contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:this.state.contents
+        });
+      }.bind(this)}></CreateContent>
+    }
     return (
       <div className="App">
         <Subject 
-        title={this.state.subject.title} 
-        sub={this.state.subject.sub} >
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub} 
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
         </Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        {/* <header>
+          <h1><a href="/" onClick={function(e){
+            console.log('event in', this);
+            return;
+            console.log(e);
+            e.preventDefault();
+            // this.state.mode = 'welcome';
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header> */}
+        <TOC 
+          onChangePage={function(){
+            alert('hi');
+            this.setState({
+              mode:'read',
+              selected_content_id:id
+            });
+          }.bind(this)} 
+          data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={function(mode){
+          this.setState({
+            mode:_mode
+          })
+        }.bind(this)}></Control>
+        {_article}
+        
       </div>
     );
   }
---
+++ 06. React/react-app/src/components/Control.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Control extends Component {
+    render() {
+      <ul>
+        <li><a href="/create" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('create');
+        }.bind(this)}>create</a></li>
+        <li><a href="/update" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('update');
+        }.bind(this)}>update</a></li>
+        <li><a href="/delete" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('delete');
+        }.bind(this)}>delete</a></li>
+      </ul>
+    }
+  }
+
+export default Control;
